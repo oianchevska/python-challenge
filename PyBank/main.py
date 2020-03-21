4698720b3ea4109223f3fa2b3ce4e24bffcc84d0
oianchevska <oianchevska@gmail.com>
2020-03-21 12:28:55 -0400

    HW-3: Added comments, formating and fixed the logic with mont

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,62 +1,67 @@
+# Import Dependencies
 import os
 import csv
 
-py_bank_path=os.path. join('.','Resources','budget_data.csv')
-with open (py_bank_path) as py_bank_data:
-    budget_reader=csv.reader(py_bank_data,delimiter=',')
-    csv.header=next(budget_reader)
 
-    #total=0
-    #sum_budget=0
-    budget_list=[]
+py_bank_path = os.path.join('.', 'Resources', 'budget_data.csv')
+
+# Open and read csv file
+with open(py_bank_path) as py_bank_data:
+    budget_reader = csv.reader(py_bank_data, delimiter=',')
+
+    # Skip the header
+    csv.header = next(budget_reader)
+
+    # Read "Profit/Losses" from file and save to separate list
+    month_list = []
+    budget_list = []
     for budget in budget_reader:
-      #total+=1
-      #sum_budget+=int(budget[1])
-      budget_list.append(float(budget[1]))
-      #print(budget[0])
-      #print (total)
-      #print (sum_budget)
-      #print (a)
+        month_list.append(budget[0])
+        budget_list.append(float(budget[1]))
 
+# Save the changes to separate list
+change_list = []
+for i in range(len(budget_list) - 1):
+    f = budget_list[i + 1] - budget_list[i]
+    change_list.append(f)
+
+# Find Total Months
+total_months = len(budget_list)
+
+# Find Total
+total = sum(budget_list)
+
+# Find Average  Change
+average_change = (sum(change_list) / len(change_list))
+
+# Find Greatest Increase in Profits
+max_profit = max(change_list)
+
+# Find the month for Greatest Increase in Profits
+max_month_profit = month_list[change_list.index(max_profit)+1]
+
+# Find Greatest Decrease in Profits
+min_profit = min(change_list)
+
+# Find the mont Greatest Decrease in Profits
+min_month_profit = month_list[change_list.index(min_profit)+1]
+
+
+# Export the results to a text file
 with open("ResultBank.txt", "w") as pyBank_result:
-    pyBank_result.write("Financial Analysis"+ "\n")
-    pyBank_result.write("-"*20+ "\n")
-    pyBank_result.write("Total months: "+ str(len(budget_list))+ "\n")
-    pyBank_result.write("Total: " + str(sum(budget_list))+ "\n")
+    pyBank_result.write("Financial Analysis" + "\n")
+    pyBank_result.write("-" * 20 + "\n")
+    pyBank_result.write("Total months: {0}".format(str(total_months)) + "\n")
+    pyBank_result.write("Total: ${0}".format(str(round(total))) + "\n")
+    pyBank_result.write("Average change: ${0}".format(str(round(average_change, 2))) + "\n")
+    pyBank_result.write("Greatest Increase in Profits: {0} (${1})".format(max_month_profit, str(round(max_profit))) + "\n")
+    pyBank_result.write("Greatest Decrease in Profits: {0} (${1})".format(min_month_profit, str(round(min_profit))) + "\n")
 
-    print("Financial Analysis")
-    print("-"*20)
-    print("Total months: "+ str(len(budget_list)))
-    print("Total: " + str(sum(budget_list)))
-
-    #a1=[int(data[1]) for data in budget_reader]
-
-    #print (sum(a1))
-   # print(len(a1))
-
-
-#     result=[]
-#     a=int(next(budget_reader)[1])
-#     print(a)
-#     for i in budget_reader:
-#         print(i)
-#         c=int(i[1])-a
-#         a=int(i[1])
-#         result.append(c)
-#
-# print(max(result))
-    #print(a)
-    change_list=[]
-    for i in range(len(budget_list)-1):
-        #print(i)
-        #print(a[i])
-        f=budget_list[i+1]-budget_list[i]
-        change_list.append(f)
-
-        average_change=(sum(change_list)/len(change_list))
-
-        #pyBank_result.write("Average change:" + str(average_change) + "\n")
-
-    print ("Average change:" + str(average_change))
-    print("Greatest Increase in Profits:" + str(max(change_list)))
-    print("Greatest Decrease in Profits:" + str(min(change_list)))
+#  Print the analysis to the terminal
+print("Financial Analysis")
+print("-" * 20)
+print("Total months: {0}".format(str(total_months)))
+print("Total: ${0}".format(str(round(total))))
+print("Average change: ${0}".format(str(round(average_change,2))))
+print("Greatest Increase in Profits: {0} (${1})".format(max_month_profit,str(round(max_profit))))
+print("Greatest Decrease in Profits: {0} (${1})".format(min_month_profit,str(round(min_profit))))
