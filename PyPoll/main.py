1975ab7962d1d66a1fa99c1617198cb915a7d50b
oianchevska <oianchevska@gmail.com>
2020-03-21 13:17:45 -0400

    HW-3: Added comments, removed debuging for PyBank

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,72 +1,50 @@
+# Import Dependencies
 import os
 import csv
 
-# Find the winning candidate with function
-def max_d(d):
-    v = list(d.values())
-    k = list(d.keys())
-    return k[v.index(max(v))]
+# Open and read csv file
+file_path = os.path.join(".", "Resources", "election_data.csv")
 
-# Open csv file
-file_path=os.path.join(".","Resources","election_data.csv")
+with open(file_path) as election_data:
 
+    reader = csv.reader(election_data, delimiter=',')
 
-with open (file_path) as election_data:
+    # Skip the header
+    header = next(reader)
 
-    reader=csv.reader(election_data,delimiter=',')
-    header=next(reader)
-    #print(header)
-
-# Calculate total number of voters
-    total_voter=0
-    candidate=dict()
-
+    # Calculate the total number of votes and for each candidate
+    total_voter = 0
+    candidate = dict()
     for data in reader:
-        total_voter+=1
+        total_voter += 1
+        candidate[data[2]] = candidate.get(data[2], 0) + 1
 
-      #
-        candidate[data[2]]=candidate.get(data[2],0)+1
+# Use lambda to retrieve data as a sorted list
+can = candidate.items()
+can = sorted(can, reverse=True, key=lambda x: x[1])
 
-    #print(candidate)
-    # Find a winner
-    fun=max_d(candidate)
+# Export the results to a text file and print to the terminal
+with open("Result.txt", "w") as pypoll_result:
 
-  # Use lambda to retrieve data as a list
-can=candidate.items()
-can=sorted(can,reverse=True,key=lambda x:x[1])
-
-# Write result in txt document
-with open("Result.txt","w") as pypoll_result:
-    pypoll_result.write("Election Results"+"\n")
-    pypoll_result.write ("-"*20+ "\n")
-    pypoll_result.write("Total Votes: "+ str(total_voter)+ "\n")
-    pypoll_result.write("-"*20+ "\n")
-
-
+    pypoll_result.write("Election Results" + "\n")
+    pypoll_result.write("-" * 25 + "\n")
+    pypoll_result.write("Total Votes: " + str(total_voter) + "\n")
+    pypoll_result.write("-" * 25 + "\n")
 
     print("Election Results")
-    print('-'*20)
+    print('-' * 25)
     print("Total Votes: " + str(total_voter))
-    print('-'*20)
+    print('-' * 25)
 
-    for cand in can:
-    #print(cand[0]+ ":" , str(round((cand[1]/total_voter)*100,3)) + "%", cand[1])
-        s=f"{cand[0]}: {cand[1]/total_voter*100:.3}% ({cand[1]})"
+    for c in can:
+        s = f"{c[0]}: {c[1]/total_voter*100:.3}% ({c[1]})"
         pypoll_result.write(s + "\n")
-
         print(s)
 
-    #print(candidate)
-    #print(total_voter)
-    #for c in candidate.items():
-        #print(c)
-    pypoll_result.write("-" * 20 + "\n")
-    pypoll_result.write("Winner: " + fun+ "\n")
-    pypoll_result.write("-" * 20 + "\n")
-    print("-"*20)
-    print("Winner: " + fun)
-    print("-" * 20)
+    pypoll_result.write("-" * 25 + "\n")
+    pypoll_result.write("Winner: " + str(can[0][0]) + "\n")
+    pypoll_result.write("-" * 25 + "\n")
 
-
-
-
+    print("-" * 25)
+    print("Winner: " + str(can[0][0]))
+    print("-" * 25)
